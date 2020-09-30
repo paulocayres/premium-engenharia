2b1081f79769454faac70e41aff2c306012812c8
unknown <paulo.cayres@gmail.com>
2020-09-30 02:03:13 -0300

    atualizaçãoo cleardb

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,34 @@
 
 // ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define( 'DB_NAME', 'wordpress' );
+//define( 'DB_NAME', 'wordpress' );
 
 /** Usuário do banco de dados MySQL */
-define( 'DB_USER', 'root' );
+//define( 'DB_USER', 'root' );
 
 /** Senha do banco de dados MySQL */
-define( 'DB_PASSWORD', '' );
+//define( 'DB_PASSWORD', '' );
 
 /** Nome do host do MySQL */
-define( 'DB_HOST', 'localhost' );
+//define( 'DB_HOST', 'localhost' );
 
 /** Charset do banco de dados a ser usado na criação das tabelas. */
-define( 'DB_CHARSET', 'utf8' );
+//define( 'DB_CHARSET', 'utf8' );
 
 /** O tipo de Collate do banco de dados. Não altere isso se tiver dúvidas. */
-define( 'DB_COLLATE', '' );
+//define( 'DB_COLLATE', '' );
+
+if(isset($_ENV[`CLEARDB_DATABASE_URL`])) {
+    $db = parse_url($_ENV[`CLEARDB_DATABASE_URL`]);
+    define(‘DB_NAME’, trim($db[`path`],`/`));
+    define(‘DB_USER’, $db[`user`]);
+    define(‘DB_PASSWORD’, $db[`pass`]);
+    define(‘DB_HOST’, $db[`host`]);
+    define(‘DB_CHARSET’, ‘utf8’);
+    define(‘DB_COLLATE’, ‘’);
+} else {
+    die(‘No Database credentials!’);
+}
 
 /**#@+
  * Chaves únicas de autenticação e salts.

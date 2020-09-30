405023521fec445584cb9195e68858cc585b012a
unknown <paulo.cayres@gmail.com>
2020-09-30 02:17:18 -0300

    atualização conexão banco

--- wp-config.php
+++ wp-config.php
@@ -39,10 +39,10 @@
 
 if(isset($_ENV[`CLEARDB_DATABASE_URL`])) {
     $db = parse_url($_ENV[`CLEARDB_DATABASE_URL`]);
-    define(‘DB_NAME’, trim($db[`path`],`/`));
-    define(‘DB_USER’, $db[`user`]);
-    define(‘DB_PASSWORD’, $db[`pass`]);
-    define(‘DB_HOST’, $db[`host`]);
+    define(‘heroku_e0fef70866a001e’, trim($db[`path`],`/`));
+    define(‘b981a537e25a0d’, $db[`user`]);
+    define(‘0688ca10’, $db[`pass`]);
+    define(‘us-cdbr-east-02.cleardb.com’, $db[`host`]);
     define(‘DB_CHARSET’, ‘utf8’);
     define(‘DB_COLLATE’, ‘’);
 } else {
